1634b77a65e4f905123a2b472b68d7192df194dd
abbosaliboev <abbosaliboyev66669999@gmail.com>
2025-03-26 20:52:17 +0900

    Fix Sidebar

--- frontend/my-app/src/components/Sidebar.js
+++ frontend/my-app/src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../assets/components/_sidebar.scss";
 import { FaBars, FaTimes, FaHome, FaChartPie, FaClock, FaArchive, FaFileAlt, FaInfoCircle, FaUser } from "react-icons/fa"; // React-icons
+import aiLogo from "../assets/images/ai-logo.png"; // images link
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false); // Sidebar ochiq-yopiq holati
@@ -17,7 +18,7 @@ const Sidebar = () => {
 
       {/* 📌 Sidebar (agar `isOpen` true bo‘lsa, chiqadi) */}
       <aside className={`sidebar ${isOpen ? "open" : ""}`}>
-        <div className="sidebar-logo">AI</div>
+        <div className="sidebar-logo"><img src={aiLogo} alt="ai-logo"/></div>
         <ul className="sidebar-menu">
           <li><FaHome /><span>Home</span></li>
           <li><FaChartPie /><span>Summary</span></li>
