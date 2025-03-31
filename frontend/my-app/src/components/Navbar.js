8298aeea75653116f9ebb61d66e95ae2d82c9db1
abbosaliboev <abbosaliboyev66669999@gmail.com>
2025-03-31 15:44:06 +0900

    test

--- frontend/my-app/src/components/Navbar.js
+++ frontend/my-app/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "../assets/components/_navbar.scss"; // Import local SCSS file
 import { FaRedo, FaGlobe, FaCog } from "react-icons/fa"; // React-icons
 
